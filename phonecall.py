b6e67f2d27f53bd448b43bd2cdf82742e5fd25e8
mikefratelli <42177196+mikefratelli@users.noreply.github.com>
2019-03-19 14:49:46 -0700

    Update phonecall.py

--- phonecall.py
+++ phonecall.py
@@ -2,13 +2,17 @@ import os
 #pip3.6 install twilio
 from twilio.rest import Client
 
+parser = argparse.ArgumentParser(description='sends phone call to specified number')
+parser.add_argument('number', help='the phone number of the agent to call')
+args = parser.parseargs()
+
 account_sid = 'os.environ["TWILIO_SID"];
 auth_token = 'os.environ["TWILIO_AUTH_TOKEN"];
 
 client = Client(account_sid, auth_token);
 
 call = client.calls.create(
-    to="<receiving phone number>",
+    to=args.number,
     from_="<twilio phone number>",
     #URL must get an http source, https won't work
     url="<hosted xml data>"
